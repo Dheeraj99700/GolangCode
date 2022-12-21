d55341475756bcd1726d28f0c997ebd37e6578dd
Dheeraj99700 <112638628+Dheeraj99700@users.noreply.github.com>
2022-12-21 09:29:01 +0530

    Problem on circle
    Computes Perimeter and Area of circle

---
+++ Day1Problems/circleproblem.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*var diameter float32 = 0
+	var area float32 = 0
+	var perimeter float32 = 0
+
+	fmt.Printf("Enter the Diameter of circle : ")
+	fmt.Scanf("%f", &diameter)
+	area = 0.25 * 3.14 * (diameter * diameter)
+	perimeter = diameter * 3.14
+	fmt.Printf("The area of circle is : %f\n", area)
+	fmt.Printf("The Perimeter of circle is : %f\n", perimeter)
+	*/
+	/*var num1, num2, num3 = 10, 20, "A"*/
+const A
+fmt.println(A)
+
+
+
+	/*fmt.Printf("%d %d %s  :newline\n", num1, num2, num3)        //newline
+	fmt.Printf("%d\t%d %s  :tabspace\n", num1, num2, num3)      //tabspace
+	fmt.Printf("\"%d\" %d %s :doublequote\n", num1, num2, num3) //doublequote
+	fmt.Printf("%d\f %d %s  :form feed\n", num1, num2, num3)    //form feed
+    */
+}
