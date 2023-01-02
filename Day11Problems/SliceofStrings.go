5d46b754f9d94f8a69013e0ee73e41425b6fcfbb
Dheeraj99700 <112638628+Dheeraj99700@users.noreply.github.com>
2023-01-02 18:56:40 +0530

    Program of slice of string

---
+++ Day11Problems/SliceofStrings.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func filter(strings []string, functionName func(string) bool) []string {
+	filteredstrings := []string{}
+	for _, stringvalue := range strings {
+		if functionName(stringvalue) {
+			filteredstrings = append(filteredstrings, stringvalue)
+		}
+	}
+	return filteredstrings
+}
+
+func StringwithB(stringvalue string) bool {
+	_ = strings.Split(stringvalue, "")
+	if stringvalue[0] == 'b' || stringvalue[0] == 'B' {
+		return true
+	}
+	return false
+
+}
+
+func Stringwith3Char(stringvalue string) bool {
+	return len(stringvalue) == 3
+
+}
+
+func main() {
+
+	strings := []string{"ant", "beetle", "Bee", "wasp", "butterfly", "fly", "moth"}
+
+	stringwithB := filter(strings, StringwithB)
+	fmt.Println(stringwithB)
+
+	stringwith3Char := filter(strings, Stringwith3Char)
+	fmt.Println(stringwith3Char)
+}
