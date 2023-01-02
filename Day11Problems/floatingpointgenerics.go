c42c0843e8beb90f6dc657575c07d28fde517783
Dheeraj99700 <112638628+Dheeraj99700@users.noreply.github.com>
2023-01-02 18:55:26 +0530

    Program Of float Generics

---
+++ Day11Problems/floatingpointgenerics.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func filter[T any](numbers []T, functionName func(T) bool) []T {
+	filteredNumbers := []T{}
+	for _, number := range numbers {
+		if functionName(number) {
+			filteredNumbers = append(filteredNumbers, number)
+		}
+	}
+	return filteredNumbers
+}
+
+func DecimalPoint(number float64) bool {
+	return math.Mod(number, 1) == 0
+}
+
+func Divisble(number float64) bool {
+	return math.Mod(number, 3) == 0
+}
+
+func main() {
+
+	numbers := []float64{1.00, 2.81, 3.00, 5.79, 6.00, 7.41, 8.0, 9.34, 1.79, 11.00, 12.00, 15.00, 5.21, 9.00}
+
+	numbersDecimalpoint := filter(numbers, DecimalPoint)
+	fmt.Println(numbersDecimalpoint)
+
+	Divisbleby3 := filter(numbers, Divisble)
+	fmt.Println(Divisbleby3)
+}
